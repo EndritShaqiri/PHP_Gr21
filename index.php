acd4baa1ede09d54244946dd336d88f76a381a63
EndritShaqiri <74931764+EndritShaqiri@users.noreply.github.com>
2021-05-14 17:52:38 +0200

    HomePage

--- index.php
+++ index.php
@@ -1,119 +1,37 @@
 <?php
-include_once "../includes/functions.php";
-include_once "../includes/connection.php";
-session_start();
-if(isset($_SESSION['author_role'])){
-	?>
+include_once "includes/functions.php";
+include_once "includes/connection.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Admin Panel</title>
+		<title>Lajmet e fundit - DARDANIA</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	</head>
 	<body>
 	
-	 <nav class="navbar navbar-dark sticky-top bg-dark p-3 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Dardania</a>
-      
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="logout.php">Sign out</a>
-        </li>
-      </ul>
-    </nav>
+    <!-- NAVBAR -->
+    <?php include_once "includes/nav.php"?>
 
-    <div class="container">
-      <div class="row">
-        <?php include_once "nav.inc.php"; ?>
+    <?php add_jumbotron() ?>
 
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dashboard</h1>
-            <h6>Pershendetje <?php echo $_SESSION['author_name']; ?> | Roli juaj eshte <?php echo $_SESSION['author_role']; ?></h6>
-          </div>
+<br>
+<div class="container">
+    <div class="card-columns">
+        <div class="card" style="width: 18rem;">
+		    <img class="card-img-top" alt="Card image cap">
+		    <div class="card-body">
+		    <h5 class="card-title">Post Title</h5>
+		    <h6 class="card-subtitle mb-2 text-muted">Author Name</h6>
+	    	<p class="card-text">Hello there!</p>
+		    <a href="#" class="btn btn-primary">Read More</a>
+		    </div>
+        </div>
+	</div>
+</div>
+    
 		
-			<div id="admin-index-form">
-			<?php
-			if(isset($_GET['message'])){
-				$msg = $_GET['message'];
-				echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-				'.$msg.'
-				  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				  </button>
-				</div>';
-			}
-			?>
-			<h1>Your Profile</h1>
-				<form method="post">
-					Name:<input name="author_name" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name"value="<?php echo $_SESSION['author_name']; ?>"><br>
-					Email:
-					<input name="author_email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"value="<?php echo $_SESSION['author_email']; ?>"><br>
-					Password:
-					<input name="author_password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"><br>
-					Your Bio:
-					<textarea name="author_bio" class="form-control" id="exampleFormControlTextarea1" rows="3"><?php echo $_SESSION['author_bio']; ?></textarea><br>
-				  <button type="submit" name="update" class="btn btn-primary">Update</button>
-				</form>
-				<?php 
-					if(isset($_POST['update'])){
-						$author_name = mysqli_real_escape_string($conn, $_POST['author_name']);
-						$author_email = mysqli_real_escape_string($conn, $_POST['author_email']);
-						$author_password = mysqli_real_escape_string($conn, $_POST['author_password']);
-						$author_bio = mysqli_real_escape_string($conn, $_POST['author_bio']);
-						
-						//checking if fields are empty
-						if(empty($author_name) OR empty($author_email) OR empty($author_bio)){
-							echo "Empty Fields";
-						}else{
-							//checking if email is valid
-							if(!filter_var($author_email, FILTER_VALIDATE_EMAIL)){
-								echo "Pleaseenter a Valid email!";
-							}else{
-								//check if password entered is new
-								if(empty($author_password)){
-									//user dont want to change his password
-									$author_id = $_SESSION['author_id'];
-									$sql = "UPDATE `author` SET author_name='$author_name', author_email='$author_email', author_bio='$author_bio' WHERE author_id='$author_id';";
-									if(mysqli_query($conn, $sql)){										
-										$_SESSION['author_name'] = $author_name;
-										$_SESSION['author_email'] = $author_email;
-										$_SESSION['author_bio'] = $author_bio;
-										header("Location: index.php?message=Record+Updated");
-										
-									}else{
-										echo "error";
-									}
-								}else{
-									//user wants to change his password
-									$hash = password_hash($author_password, PASSWORD_DEFAULT);
-									$author_id = $_SESSION['author_id'];
-									$sql = "UPDATE `author` SET author_name='$author_name', author_email='$author_email', author_bio='$author_bio', author_password='$hash' WHERE author_id='$author_id';";
-									if(mysqli_query($conn, $sql)){										
-										session_unset();
-										session_destroy();
-										header("Location: login.php?message=Record+Updated+You+may+login+again+now");
-										
-										
-									}else{
-										echo "error";
-									}
-								}
-							}
-						}
-					}
-				?>
-			</div>
-          </div>
-        </main>
-      </div>
-    </div>
 	</body>
 </html>
-	<?php
-}else{
-	header("Location: login.php?message=Please+Login");
-}
-?>
