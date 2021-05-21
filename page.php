17d19165974c006332bbf7832442bc2c21ac3774
Fisnik Mustafa <fisnikmustafa00@gmail.com>
2021-05-21 20:49:38 +0200

    added mail sending functionality, +fixed some layout mistakes

--- page.php
+++ page.php
@@ -23,6 +23,39 @@ if(!isset($_GET['id'])){
 				
 				?>
 				
+				<!-- email sending -->
+
+				<?php
+                    
+                    $recipient = "";
+                    //if user clicks the send button
+                    if(isset($_POST['send_email'])){
+                        //access user entered data
+					   $senderName = $_POST['contact_name'];
+                       $recipient = $_POST['contact_email'];
+                       $subject = $_POST['contact_subject'];
+					   $message = "Message from: $senderName\n\n";
+                       $message .= $_POST['contact_message'];
+                       $sender = "From: testinggacc630@gmail.com";
+                       //if user leaves an empty field
+                       if(empty($recipient) || empty($subject) || empty($message)){
+                           //display an alert message if one of the fields is empty -->
+                           header("Location: page.php?id=$id&message=All+inputs+are+required!");
+                        }else{
+                            // PHP function to send mail
+                           if(mail($recipient, $subject, $message, $sender)){
+                            
+                            header("Location: page.php?id=$id&message=Your+mail+was+successfully+sent!");
+
+                           $recipient = "";
+                           }else{
+                            
+                            header("Location: page.php?id=$id&message=Failed+to+send+your+mail!");
+
+                           }
+                       }
+                    }
+                ?>
 				
 <!DOCTYPE html>
 <html lang="en">
@@ -31,6 +64,8 @@ if(!isset($_GET['id'])){
 		<meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="style/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 	
@@ -43,6 +78,18 @@ if(!isset($_GET['id'])){
 		<div class="container">
 			<h1 style="width:100%;background-color:grey;padding-top:25px;padding-bottom:25px;text-align:center;color:white"><?php echo $page_title2; ?></h1>
 			<hr>
+
+			<?php
+			if(isset($_GET['message'])){
+				$msg = $_GET['message'];
+				echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+				'.$msg.'
+				  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				  </button>
+				</div>';
+			}
+			?>
 			
 			<p><?php echo $page_content; ?></p><br>
 
@@ -73,19 +120,21 @@ function showHint(str) {
    }
 }
 </script>
-
-
-			
+	
 		</div>
 		
 	
-</body>
-</html>
-				
-				
-				<?php
+	
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/scroll.js"></script>
+		</body>
+	</html>
+					
+					
+					<?php
+				}
 			}
 		}
 	}
-}
-?>
+	?>
